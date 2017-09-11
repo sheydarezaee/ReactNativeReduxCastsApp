5267ca73ca0232dc3a398c8de6db95aa0469da57
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-09-11 12:14:51 -0700

    changes text on the button to make the component more reusable for future uses

--- albums/src/components/button.js
+++ albums/src/components/button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
   return (
     <TouchableOpacity 
@@ -9,7 +9,7 @@ const Button = ({ onPress }) => {
       style={buttonStyle}
     >
       <Text style={textStyle}>
-        Click me!!!
+        {children}
       </Text>
     </TouchableOpacity>
   );
