80dc58b24ffaa1a3ca23a3fd71346d238c71bc8e
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-09-10 19:05:36 -0700

    updates layout styles CardSection artist image  with FlexBox

--- albums/src/components/albumDetail.js
+++ albums/src/components/albumDetail.js
@@ -3,19 +3,26 @@ import { Text, View, Image } from 'react-native';
 import Card from './card';
 import CardSection from './cardSection';
 
-const AlbumDetail = (props) => {
-  console.log(props.album);
+const AlbumDetail = ({ album }) => {
+
+  const {title, artist, thumbnail_image} = album; //yay destructuring
+  const { headerContentStyle, thumbnailStyle} = styles;
+  
   return(
     <Card>
 
       <CardSection>
+
         <View>
-          <Image />
+          <Image 
+            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle} 
+          />
         </View>
 
-        <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
 
       </CardSection>
@@ -28,8 +35,12 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
-}
+};
 
 export default AlbumDetail;
 
@@ -38,3 +49,13 @@ export default AlbumDetail;
 //wrap image and teh texts in two sepeare View tags and style it
 //use justifyContent: 'flex-start' to push both containers to the left side
 //but default is verical, so flexDirection:'row' solves this
+
+
+//whenever we have multiple refereneces to props object, like here , we have three
+//props.album, it's a good time to destructure that property out of the argument of props ex. line 6 here
+
+
+//react-native images will bot expand to fill the space by default,
+// instead we have to manually adding a styling rule that tells the image tag
+// height and width ,... to be able to render that image on the page
+//and dont forget 
