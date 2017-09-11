0b3d25178aea2ec0b1e459b490d8c1b7353ce5e1
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-09-10 18:27:55 -0700

    updates layout styles CardSection header with FlexBox

--- albums/src/components/albumDetail.js
+++ albums/src/components/albumDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Text, Image } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './card';
-import CardSection from './CardSection';
+import CardSection from './cardSection';
 
 const AlbumDetail = (props) => {
   console.log(props.album);
@@ -9,11 +9,32 @@ const AlbumDetail = (props) => {
     <Card>
 
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image />
+        </View>
+
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
+
       </CardSection>
-      
+
     </Card>
   );
 };
 
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
+}
+
 export default AlbumDetail;
+
+//to manage styling for CardSection header, which consists of 
+//an image on the left and Album Name + Artist Name on the right
+//wrap image and teh texts in two sepeare View tags and style it
+//use justifyContent: 'flex-start' to push both containers to the left side
+//but default is verical, so flexDirection:'row' solves this
